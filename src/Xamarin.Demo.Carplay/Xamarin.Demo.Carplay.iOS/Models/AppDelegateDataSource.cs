03c1f6a1e89f1dda0bd2333c277f399c41cb766f
Suplanus <info@suplanus.de>
2020-02-29 16:25:28 +0100

    Items

--- src/Xamarin.Demo.Carplay/Xamarin.Demo.Carplay.iOS/Models/AppDelegateDataSource.cs
+++ src/Xamarin.Demo.Carplay/Xamarin.Demo.Carplay.iOS/Models/AppDelegateDataSource.cs
@@ -1,26 +1,84 @@
 ﻿using System;
+using CoreFoundation;
 using Foundation;
 using MediaPlayer;
+using UIKit;
 
 namespace Xamarin.Demo.Carplay.iOS.Models
 {
-    internal class PlayableContentDataSource : MPPlayableContentDataSource
+  internal class PlayableContentDataSource : MPPlayableContentDataSource
+  {
+    string _defaultImage = "https://cuterdio.com/user/themes/landed/images/pic01.png";
+
+    public override MPContentItem ContentItem(NSIndexPath indexPath)
     {
-        public override MPContentItem ContentItem(NSIndexPath indexPath)
-        {
-            return null;
-        }
-
-        public override nint NumberOfChildItems(NSIndexPath indexPath)
-        {
-            return 0;
-        }
-
-        public override void BeginLoadingChildItems(NSIndexPath indexPath, Action<NSError> completionHandler)
-        {
-            
-        }
-
-        
+      if (indexPath.Length == 1)
+      {
+        var item = new MPContentItem("Stations");
+        item.Title = "Stations";
+        item.Container = true;
+        item.Playable = false;
+        item.Artwork = GetImageFromUrl(_defaultImage);
+        return item;
+      }
+      else
+      {
+        // SubItems
+        var item = new MPContentItem("Test" + indexPath.Length);
+        item.Title = "TITLE" + indexPath.Length;
+        item.Subtitle = "SUBTITLE" + indexPath.Length;
+        item.Playable = true;
+        item.StreamingContent = true;
+        item.Artwork = GetImageFromUrl(_defaultImage);
+        return item;
+      }
     }
+
+    public override nint NumberOfChildItems(NSIndexPath indexPath)
+    {
+      if (indexPath.GetIndexes().Length == 0)
+      {
+        return 1;
+      }
+      return 3;
+    }
+
+    public override void BeginLoadingChildItems(NSIndexPath indexPath, Action<NSError> completionHandler)
+    {
+
+    }
+
+    private MPMediaItemArtwork GetImageFromUrl(string url)
+    {
+      UIImage image = null;
+      NSUrlSession session = NSUrlSession.SharedSession;
+      NSUrlSessionDataTask dataTask = session.CreateDataTask(new NSUrlRequest(new NSUrl(url)),
+                                                             (data, response, error) =>
+                                                             {
+                                                               if (response != null)
+                                                               {
+                                                                 image = GetImageFromUrl(data);
+                                                               }
+                                                             });
+      dataTask.Resume();
+
+      if (image != null)
+      {
+        return new MPMediaItemArtwork(image);
+      }
+      return null;
+    }
+
+    private static UIImage GetImageFromUrl(NSData data)
+    {
+      UIImage image = null;
+      DispatchQueue.MainQueue.DispatchAsync(() =>
+      {
+        image = UIImage
+          .LoadFromData(
+            data);
+      });
+      return image;
+    }
+  }
 }
